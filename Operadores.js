13380399761add2a229d56fc607892e6a8be0413
Gabriel Carvalho <79951027+Carvalhocodes@users.noreply.github.com>
2025-06-25 10:36:39 -0300

    Create Operadores.js
    script operadores

---
+++ Operadores.js
@@ -0,0 +1,48 @@
+// Script de Soma!
+let valorChurrascoGabriel = 30
+let valorChurrascoGiovanna = 50 
+let valorChurrascoBeni = 100
+let resultado = valorChurrascoGabriel + valorChurrascoGiovanna + valorChurrascoBeni
+console.log  ("valor total da soma é : " + resultado )
+
+
+// Script de Multiplicação
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log ("O resultado da multiplicação é:" + produto)
+
+// Script de Divisão
+let notaMercado = 100
+let pessoaParaDividir = 2
+console.log ("Divisão para cada um pagar é de: " + notaMercado / pessoaParaDividir + " Reais " )
+
+// Script de Sobra
+let calculo = 10 % 3
+console.log ("Calculo de sobra "  + calculo)
+
+//script de incremento e decremento 
+let contador = 1
+// '++' incremento 
+contador++ 
+contador++
+
+// '--' decremento 
+contador--
+contador--
+contador-
+console.log (contador)
+
+// Operadores de atribuição
+let preco = 10
+preco += 5  // mesma coisa que preco = preco +5 / código fica mais fácil de ler! 
+preco -= 5 // mesma coisa que preco = preco -5 
+console.log (preco)
+
+//operações aritmeticas 
+let numero = 2 * ((5 + 5 ) - 10)
+// Aqui ele faz primeiro o 5+5 depois faz o -10 e depois multiplica por 2 
+console.log (numero) 
+
+
+
