9f30587f99c8099f4a8ad37a6a04e64924714ee8
Gabriel Carvalho <79951027+Carvalhocodes@users.noreply.github.com>
2025-06-25 10:36:27 -0300

    Create maiorouMenor.js
    Subir script maior e menor ou igual

---
+++ maiorouMenor.js
@@ -0,0 +1,9 @@
+// Comparação de maior igual ">=" ou menor igual "<="
+let idadeMinima = 18
+let idadeUsuario = 17
+let idadePermitida = idadeUsuario >= idadeMinima
+console.log ("idade permitida :" + idadePermitida)
+
+//JavaScript é uma linguagem Case sensitive, elá diferencia palavras com Maiuscula  e minuscula. 
+let texto = "Gabriel"
+console.log (texto === "Gabriel")
